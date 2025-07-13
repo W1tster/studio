4936ffcad5a1a5bf51446d3086b325b503bdd481
Vaibhav Kulkarni <vaiku2656@gmail.com>
2025-07-13 11:59:12 +0000

    make the top search bar longer

--- src/components/app-header.tsx
+++ src/components/app-header.tsx
@@ -28,7 +28,7 @@ export function AppHeader() {
             <Input
               type="search"
               placeholder="Search by keywords, tags..."
-              className="w-full appearance-none bg-background pl-8 shadow-none md:w-2/3 lg:w-1/3"
+              className="w-full appearance-none bg-background pl-8 shadow-none md:w-2/3 lg:w-1/2"
             />
           </div>
         </form>
