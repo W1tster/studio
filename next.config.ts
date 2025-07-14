6218274cb538bf110642afd339248496d242ec8b
W1tster <vaiku2656@gmail.com>
2025-07-15 00:49:19 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -19,7 +19,7 @@ const nextConfig: NextConfig = {
     ],
   },
   // genkit packages are CJS, so we need to tell Next.js to not try to bundle them
-  // and instead treat them as external packages.
+  // and instead treat them as external packages
   // see: https://nextjs.org/docs/app/api-reference/next-config-js/serverComponentsExternalPackages
   serverComponentsExternalPackages: [
     '@genkit-ai/ai',
